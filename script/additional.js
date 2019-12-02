4fb9d11ad15ba9793ef6ed736eafdc618b5a89b6
Shamil Magomedov <shomainkognito@yandex.ru>
2019-12-02 17:24:09 +0300

    additional task

---
+++ script/additional.js
@@ -0,0 +1,8 @@
+let num = "266219";
+num.split(' ');
+num = num[0] * num[1] * num[2] * num[3] * num[4] * num[5];
+console.log(num);
+num = num ** 3;
+num = num + '';
+num.split('');
+console.log( num.substr(0, 2) );
